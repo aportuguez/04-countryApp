08b47aec3516a9113474bfff28e7f9f76a6d3732
Adrian Portuguez <designeradrian@gmail.com>
2024-11-14 08:17:11 -0600

    lesson 126

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
-export class ServiceNameService {
+export class CountriesService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
@@ -13,6 +13,9 @@ export class ServiceNameService {
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`
     return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(error => of([]))
+      );
   }
 
 }
